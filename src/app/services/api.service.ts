4b05d9665570ab8ab6130d5dc29d869c96120a9e
drobunator@gmail.com <shumeiko11824141>
2020-03-14 21:18:10 +0200

    added service, have some problem whit counter in tasks

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: AngularFirestore) {
+  }
+
+  get() {
+    return this.http.collection('tasks').snapshotChanges();
+  }
+
+  post(task: object) {
+    const id = this.http.createId();
+    return this.http.collection('tasks').doc(id).set({
+      ...task,
+      id
+    });
+  }
+
+  update(id, value) {
+    return this.http.collection('tasks').doc(id).update(value);
+  }
+
+  delete(id: string) {
+    return this.http.collection('tasks').doc(id).delete();
+  }
+}
---
+++ src/app/services/confirm.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {ApiService} from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfirmService {
+  activeConfirm = false;
+  taskId = '';
+  title = 'Are you sure?';
+
+  constructor(private api: ApiService) {
+  }
+
+  delete(value: boolean) {
+    if (value) {
+      this.activeConfirm = false;
+      this.api.delete(this.taskId)
+        .then(_ => console.log('Task deleted'))
+        .catch(_ => console.log('Somthing whent wrong!!!'));
+    } else {
+      this.activeConfirm = false;
+    }
+
+  }
+
+  set stat(value: boolean) {
+    this.activeConfirm = value;
+  }
+
+  set id(value) {
+    this.taskId = value;
+  }
+
+  set setTitle(value: string) {
+    this.title = value;
+  }
+}
